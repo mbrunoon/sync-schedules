6fec893269e20e0a54f78e9898fef2ec7edd2a6f
Marcos Bruno <contato@marcosbruno.com>
2022-02-26 08:27:54 -0300

    add: users rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:all) do
+    @userOne = User.create(email: "one@user.com", password: "12345678")
+    @userTwo = User.create(email: "two@user.com", password: "12345678")
+  end
+
+  let(:valid_attributes){
+    {
+      :email => "three@user.com", 
+      :password => "12345678"
+    }
+  }
+
+  it "validate user Three attributes" do
+    @userThree = User.new(valid_attributes)
+    expect(@userThree).to be_valid
+  end
+
+  it "validate uniqueness of email" do
+    @userThree = User.new(valid_attributes)
+    @userThree.email = @userOne.email
+    expect(@userThree).to_not be_valid
+  end
+
+  it "validate lengt of pasword" do
+    @userThree = User.new(valid_attributes)
+    @userThree.password = "123"
+    expect(@userThree).to_not be_valid
+  end
+
+
 end
